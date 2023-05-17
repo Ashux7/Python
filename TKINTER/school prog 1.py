02c3f0a88eee129bc9e753d2475dd6e5be76b17e
Ashutosh <prajapatiashu777@gmail.com>
2023-05-17 21:12:18 +0530

    --

--- TKINTER/school prog 1.py
+++ TKINTER/school prog 1.py
@@ -1,21 +1,19 @@
+import pandas as pd
 from tkinter import *
-import os
+
 win = Tk()
-win.geometry('200x200')
-win.title(os.getcwd())
+win.geometry('500x500')
 
+s1={'name':'Ashu','class':'XII','sec':'A','gen':'M'}
+s2={'name':'Tasmay','class':'XII','sec':'B','gen':'M'}
+s3={'name':'Lavish','class':'XII','sec':'B','gen':'M'}
+s4={'name':'Hemal','class':'XII','sec':'C','gen':'M'}
+s5={'name':'Manish','class':'XII','sec':'C','gen':'M'}
+
+df = pd.DataFrame([s1,s2,s3,s4,s5],index=['Student 1','Student 2','Student 3','Student 4','Student 5'])
+
+for i in df:
+    print(df)
     
-
-txt1 = Label(win,text='Enter Text').pack()
-ans_txt1 = StringVar()
-entry_txt1 = Entry(win,width=30,textvariable=ans_txt1).pack()
-
-def output():
-    ans = ans_txt1.get()
-    Label(win,text=ans).pack()
-
-btn1 = Button(win,text="SUBMIT",command=output).pack()
-
-
 
 win.mainloop()
